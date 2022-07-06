a365e890bf9556fecb007888fada998da3d8d28a
Chang1235 <dlckdgml1235@naver.com>
2022-07-06 23:03:33 +0900

    docs: Create new file
    I solve backjoon_4673.

--- function/BJ_4673.java
+++ function/BJ_4673.java
@@ -1,5 +1,26 @@
 package function;
 
+import java.lang.reflect.Method;
+
+
 public class BJ_4673 {
+	int[] SelfNumber = new int[10001];
+	int count = 1;
+	int Self = 0;
+	int i = 0; 
+	
+	public void selfNumber(int n) {
+		
+		while(count <10000) {
+			Self = count;
+			i = count;
+			while(i != 0) {
+				Self += i % 10;
+				i /= 10;
+			}
+			if(Self < 10000)
+			count++;
+		}
+	}
 
 }
