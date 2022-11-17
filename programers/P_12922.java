62c5db1a19ebbc5abf294341da1335fdd9761389
WindowH22 <dlckdgml1235@naver.com>
2022-11-17 18:56:10 +0900

    docs: Create new file

---
+++ programers/P_12922.java
@@ -0,0 +1,17 @@
+package programers;
+
+public class P_12922 {
+    public String solution(int n) {
+        String answer = "";
+
+        for (int i = 0; i < n ; i++) {
+            if (i % 2 == 0) {
+                answer += "수";
+            } else {
+                answer += "박";
+            }
+        }
+
+        return answer;
+    }
+}
