c258c4bcf378ad2c215fc72fc1b5e0f73f51e00c
WindowH22 <dlckdgml1235@naver.com>
2022-11-21 15:18:23 +0900

    docs: Create new file at morning

---
+++ programers/P_12943.java
@@ -0,0 +1,31 @@
+package programers;
+
+public class P_12943 {
+    public int solution(int num) {
+
+        long n = num;
+        int count = 0;
+
+        while(n != 1){
+            if(n%2 == 0 ){
+                n = n/2;
+                System.out.println(n);
+            }else{
+                n = (n*3)+1;
+                System.out.println(n);
+            }
+            count ++;
+            if(count >=500){
+                return -1;
+            }
+        }
+
+
+        return count;
+
+    }
+    public static void main(String[] args){
+        P_12943 s = new P_12943();
+        System.out.println(s.solution(6));
+    }
+}
