c7371d72ec282041cea6356c36eb8a69957b5909
WindowH22 <dlckdgml1235@naver.com>
2022-11-22 23:33:49 +0900

    docs: Create new file at afternoon

---
+++ programers/P_76501.java
@@ -0,0 +1,13 @@
+package programers;
+
+public class P_76501 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.length ; i++) {
+            answer += absolutes[i]*(signs[i]? 1:-1);
+        }
+
+        return answer;
+    }
+}
