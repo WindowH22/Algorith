de5fd25c943ee491800d3e4d49e2b78aa01c9095
WindowH22 <dlckdgml1235@naver.com>
2023-02-15 20:39:30 +0900

    feat:[lv1] 구명보트 - 문제출처 https://school.programmers.co.kr/learn/courses/30/lessons/42885

---
+++ programers/lv2/P_42885.java
@@ -0,0 +1,28 @@
+package programers.lv2;
+
+import java.util.Arrays;
+
+public class P_42885 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+
+        int min = 0;
+        for (int max = people.length-1 ; min <= max ; max--) {
+            if(people[min] + people[max] <= limit){
+                min ++;
+            }
+            answer ++;
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] people = {70,50,80,50};
+
+
+    }
+}
